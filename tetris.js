934169d633836b25a24a4a193a418d5616a01951
kedanco <kelvinchowjunjie120@hotmail.com>
2018-12-18 02:19:29 +0800

    Fixed Tetris boundary, Added drawing matrix when tiles reach.

--- tetris.js
+++ tetris.js
@@ -5,6 +5,18 @@ context.scale(20, 20);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
+function collide(arena, player) {
+	const [m, o] = [player.matrix, player.pos];
+	for (let y = 0; y < m.length; y++) {
+		for (let x = 0; x < m[y].length; x++) {
+			if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 function createMatrix(w, h) {
 	const matrix = [];
 	while (h--) {
@@ -16,6 +28,7 @@ function createMatrix(w, h) {
 function draw() {
 	context.fillStyle = "#000";
 	context.fillRect(0, 0, canvas.width, canvas.height);
+	drawMatrix(arena, { x: 0, y: 0 });
 	drawMatrix(player.matrix, player.pos);
 }
 
@@ -30,9 +43,32 @@ function drawMatrix(matrix, offset) {
 	});
 }
 
+// Copies updated player values (upon input) into the arena
+function merge(arena, player) {
+	player.matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				arena[y + player.pos.y][x + player.pos.x] = value;
+			}
+		});
+	});
+}
+
 function playerDrop() {
 	player.pos.y++;
+	if (collide(arena, player)) {
+		player.pos.y--;
+		merge(arena, player);
+		player.pos.y = 0;
+	}
 	dropCounter = 0;
+}
+
+function playerMove(dir) {
+	player.pos.x += dir;
+	if (collide(arena, player)) {
+		player.pos.x -= dir;
+	}
 }
 
 let dropCounter = 0;
@@ -52,6 +88,8 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
 	pos: { x: 5, y: 5 },
 	matrix: matrix
@@ -59,9 +97,9 @@ const player = {
 
 document.addEventListener("keydown", event => {
 	if (event.keyCode === 37) {
-		player.pos.x--;
+		playerMove(-1);
 	} else if (event.keyCode === 39) {
-		player.pos.x++;
+		playerMove(1);
 	} else if (event.keyCode === 40) {
 		playerDrop();
 	}
