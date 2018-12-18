a379835efd5830d0195ebddc16db169c8e0250d3
kedanco <kelvinchowjunjie120@hotmail.com>
2018-12-18 13:30:20 +0800

    Game Completed

--- tetris.js
+++ tetris.js
@@ -5,6 +5,24 @@ context.scale(20, 20);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
+function arenaSweep() {
+	let rowCount = 1;
+	outer: for (let y = arena.length - 1; y > 0; y--) {
+		for (let x = 0; x < arena[y].length; x++) {
+			if (arena[y][x] === 0) {
+				continue outer;
+			}
+		}
+
+		const row = arena.splice(y, 1)[0].fill(0);
+		arena.unshift(row);
+		y++;
+
+		player.score += rowCount * 10;
+		rowCount *= 2;
+	}
+}
+
 function collide(arena, player) {
 	const [m, o] = [player.matrix, player.pos];
 	for (let y = 0; y < m.length; y++) {
@@ -25,6 +43,24 @@ function createMatrix(w, h) {
 	return matrix;
 }
 
+function createPiece(type) {
+	if (type === "T") {
+		return [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
+	} else if (type === "O") {
+		return [[2, 2], [2, 2]];
+	} else if (type === "L") {
+		return [[0, 3, 0], [0, 3, 0], [0, 3, 3]];
+	} else if (type === "J") {
+		return [[0, 4, 0], [0, 4, 0], [4, 4, 0]];
+	} else if (type === "I") {
+		return [[0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0]];
+	} else if (type === "S") {
+		return [[0, 6, 6], [6, 6, 0], [0, 0, 0]];
+	} else if (type === "Z") {
+		return [[7, 7, 0], [0, 7, 7], [0, 0, 0]];
+	}
+}
+
 function draw() {
 	context.fillStyle = "#000";
 	context.fillRect(0, 0, canvas.width, canvas.height);
@@ -36,7 +72,7 @@ function drawMatrix(matrix, offset) {
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value !== 0) {
-				context.fillStyle = "red";
+				context.fillStyle = colors[value];
 				context.fillRect(x + offset.x, y + offset.y, 1, 1);
 			}
 		});
@@ -59,7 +95,9 @@ function playerDrop() {
 	if (collide(arena, player)) {
 		player.pos.y--;
 		merge(arena, player);
-		player.pos.y = 0;
+		playerReset();
+		arenaSweep();
+		updateScore();
 	}
 	dropCounter = 0;
 }
@@ -70,6 +108,68 @@ function playerMove(dir) {
 		player.pos.x -= dir;
 	}
 }
+
+function playerReset() {
+	const pieces = "ILJOTSZ";
+	player.matrix = createPiece(pieces[(pieces.length * Math.random()) | 0]);
+	player.pos.y = 0;
+	player.pos.x =
+		((arena[0].length / 2) | 0) - ((player.matrix[0].length / 2) | 0);
+
+	if (collide(arena, player)) {
+		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
+	}
+}
+
+function playerRotate(dir) {
+	const pos = player.pos.x;
+	let offset = 1;
+	rotate(player.matrix, dir);
+	while (collide(arena, player)) {
+		player.pos.x += offset;
+		offset = -(offset + (offset > 0 ? 1 : -1));
+		if (offset > player.matrix[0].length) {
+			rotate(player.matrix, -dir);
+			player.pos.x = pos;
+			return;
+		}
+	}
+}
+
+//To rotate tetris block, we need to do transposition:
+// Changing the rows into columns
+// Reverse each row to get a rotated matrix
+// Transpose + Reverse = Rotate
+function rotate(matrix, dir) {
+	for (let y = 0; y < matrix.length; y++) {
+		for (let x = 0; x < y; x++) {
+			[matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+		}
+	}
+
+	if (dir > 0) {
+		matrix.forEach(row => row.reverse());
+	} else {
+		matrix.reverse;
+	}
+}
+
+function updateScore() {
+	document.getElementById("score").innerText = player.score;
+}
+
+const colors = [
+	null,
+	"#FF0D72",
+	"#0DC2FF",
+	"#0DFF72",
+	"#F538FF",
+	"#FF8E0D",
+	"#FFE138",
+	"#3877FF"
+];
 
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -92,7 +192,8 @@ const arena = createMatrix(12, 20);
 
 const player = {
 	pos: { x: 5, y: 5 },
-	matrix: matrix
+	matrix: createPiece("T"),
+	score: 0
 };
 
 document.addEventListener("keydown", event => {
@@ -102,7 +203,13 @@ document.addEventListener("keydown", event => {
 		playerMove(1);
 	} else if (event.keyCode === 40) {
 		playerDrop();
+	} else if (event.keyCode === 81) {
+		playerRotate(-1);
+	} else if (event.keyCode === 87) {
+		playerRotate(1);
 	}
 });
 
+playerReset();
+updateScore();
 update();
